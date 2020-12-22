14ca21f0eadaf58f6d94bcae821707b43f765f9d
ccskys <429657630@qq.com>
2020-12-22 11:54:18 +0800

    hello

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def main():
-  # print("Keep it logically awesome.")
+    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+    # f = open("quotes.txt")
+    # quotes = f.readlines()
+    # f.close()
 
-  #print(quotes)
+    # print(quotes)
 
-if __name__== "__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
